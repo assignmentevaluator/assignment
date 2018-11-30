7fc90ca0c9a185df66d0eb62bd6f96d969f53038
Naveen <mnaveenkumar2009@gmail.com>
2018-11-30 16:50:59 +0530

    1

--- final.py
+++
@@ -1,9 +0,0 @@
-import os,time
-
-os.system("git add .")
-os.system("git commit -m \"1\"")
-
-os.system("git push")
-
-os.system("assignmentevaluator")
-os.system("nnnn4312")
--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "1"
 git push
-assignmentevaluator
-nnnn4312
+# assignmentevaluator
+# nnnn4312
